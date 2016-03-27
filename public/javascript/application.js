f8a2a592917f618ea0fbf3126016c17e828dc11d
Matt Ruttan <matthew.ruttan5@gmail.com>
2016-03-27 23:08:37 +0000

    leaderboard page finished

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-});
-
+$(document).ready(function() 
+    { 
+        $("#myTable").tablesorter(); 
+    } 
+); 
